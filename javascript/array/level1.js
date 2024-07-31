bfc2e4467a0ea6bc1d2b15f703cea78375520b93
ajay <azaydhital1@gmail.com>
2024-07-31 07:44:34 +0545

    length and reverse

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -3,3 +3,9 @@ const arr =[3,4,5]
 //find the length of the array 
 //extected outout 3
 console.log(arr.length)
+
+//Q2
+//reverse the array 
+//expected output is[5,4,3]
+
+console.log(arr.reverse());
